d8456a44d11a824f05cd579cdee8a6abab633a11
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-15 10:46:47 -0400

    Adding logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,6 @@ public class App {
     public static void main(String[] args) {
 
         logic();
-
     }
 
 
@@ -15,6 +14,12 @@ public class App {
         Scanner scan = new Scanner(System.in);
         int userGuess = scan.nextInt();
         int secretNumber = 5;
+
+        while (userGuess != secretNumber){
+            System.out.println("I have chosen a number between 1 and 10. Try to guess it.\n");
+            System.out.print("Your guess:");
+             userGuess = scan.nextInt();
+        }
 
 
 
